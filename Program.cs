ec30d0c1f35ce14174c04a4d7d1f3827a532092b
zvaruntomar <vrnkmr119@gmail.com>
2023-09-02 13:09:42 +0530

    added interlocked

--- Program.cs
+++ Program.cs
@@ -1,85 +1,116 @@
 ﻿namespace MultiThreading;
 class Program
 {
+    public static int sum = 0;
     static void Main(string[] args)
     {
-        Console.WriteLine("Main Thread Started");
+        // Console.WriteLine("Main Thread Started");
 
-        NumberHelper nHelper = new NumberHelper(10);
+        // NumberHelper nHelper = new NumberHelper(10);
 
-        // It is not TypeSafe
-        // ParameterizedThreadStart ptStart = new ParameterizedThreadStart(nHelper.Method1);
+        // // It is not TypeSafe
+        // // ParameterizedThreadStart ptStart = new ParameterizedThreadStart(nHelper.Method1);
 
-        //TypeSafe
-        ThreadStart tStart = new ThreadStart(nHelper.Method1);
+        // //TypeSafe
+        // ThreadStart tStart = new ThreadStart(nHelper.Method1);
 
-        Thread t1 = new Thread(tStart){
-            Name = "Method1 Thread"
+        // Thread t1 = new Thread(tStart){
+        //     Name = "Method1 Thread"
+        // };
+
+        // ThreadStart t2Start =  Method2;
+
+        //  Thread t2 = new Thread(t2Start){
+        //     Name = "Method2 Thread"
+        // };
+
+        // //Used of Anonymous Function
+        // ThreadStart t3Start = delegate() { Method3();};
+
+        //  Thread t3 = new Thread(t3Start){
+        //     Name = "Method3 Thread"
+        // };
+
+        // t1.Start();
+        // //t1.Start("Varun"); // Return error as String cannot work on loop due to type safety
+        // t2.Start();
+        // t3.Start();
+
+        // Console.WriteLine("Main Thread Finished");
+
+        Thread t1 = new Thread(Addition){
+            Name = "Addition"
         };
 
-        ThreadStart t2Start =  Method2;
-
-         Thread t2 = new Thread(t2Start){
-            Name = "Method2 Thread"
+        Thread t2 = new Thread(Addition){
+            Name = "Addition"
         };
 
-        //Used of Anonymous Function
-        ThreadStart t3Start = delegate() { Method3();};
-
-         Thread t3 = new Thread(t3Start){
-            Name = "Method3 Thread"
+        Thread t3 = new Thread(Addition){
+            Name = "Addition"
         };
 
         t1.Start();
-        //t1.Start("Varun"); // Return error as String cannot work on loop due to type safety
         t2.Start();
         t3.Start();
 
-        Console.WriteLine("Main Thread Finished");
-    }
+        t1.Join();
+        t2.Join();
+        t3.Join();
 
-     public static void Method2(){
+        Console.WriteLine("Total Sum is - "+ sum);
+        Console.ReadLine();
 
-        Console.WriteLine("Method 2 started");
-
-        for (int i = 0; i <10;i++){
-            Console.WriteLine(i);
-        }
-
-        Console.WriteLine("Method 2 finished");
 
     }
 
-     public static void Method3(){
+    // public static void Method2(){
 
-        Console.WriteLine("Method 3 started");
+    //     Console.WriteLine("Method 2 started");
 
-        for (int i = 0; i <10;i++){
-            Console.WriteLine(i);
+    //     for (int i = 0; i <10;i++){
+    //         Console.WriteLine(i);
+    //     }
+
+    //     Console.WriteLine("Method 2 finished");
+
+    // }
+
+    // public static void Method3(){
+
+    //     Console.WriteLine("Method 3 started");
+
+    //     for (int i = 0; i <10;i++){
+    //         Console.WriteLine(i);
+    //     }
+
+    //     Console.WriteLine("Method 3 finished");
+    // }
+
+    public static void Addition(){
+        for(int i=0;i<5000;i++){
+            Interlocked.Increment(ref sum);
         }
-
-        Console.WriteLine("Method 3 finished");
-
     }
 }
 
 
-public class NumberHelper{
+// public class NumberHelper{
     
-    private int _number;
+//     private int _number;
 
-    public NumberHelper(int number){
-        _number = number;
-    }
+//     public NumberHelper(int number){
+//         _number = number;
+//     }
 
-    public void Method1(){
+//     public void Method1(){
 
-        Console.WriteLine("Method 1 started");
+//         Console.WriteLine("Method 1 started");
 
-        for (int i = 0; i < _number; i++) {
-            Console.WriteLine(i);
-        }
+//         for (int i = 0; i < _number; i++) {
+//             Console.WriteLine(i);
+//         }
 
-        Console.WriteLine("Method 1 finished");
-    }
-}
+//         Console.WriteLine("Method 1 finished");
+//     }
+// }
